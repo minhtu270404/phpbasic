5dbbe74671ad44debd8a044168b8d62b2bcfae59
minhtu270404 <minhtu270404>
2025-05-20 20:01:17 +0700

    aaa

--- lesson4.php
+++ lesson4.php
@@ -2,8 +2,14 @@
 
 <section class="main">
     <?php
+    $number = 9;
+    echo "Can ban 2 cua $number " . " la: " . sqrt($number);// tinh can bac hai ham cua can bac hai la sqrt
     
+    $String = "nguyen minh tu";//khai bao bien
+    echo strtoupper($String);// strtoupper la phuong thuc viet hoa
 
+    $abc = array("hh", "ee", "aa");//khai bao ham
+    print_r(array_reverse($abc));//array_reverse la phuong thuc dao nguoc, print la in ham ra
     ?>
 
 </section>
